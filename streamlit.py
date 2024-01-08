9c6cfdbc867795b9e7060b865d404184759bbcde
SirawithPra <sirawith.pra@gmail.com>
2024-01-08 19:51:32 +0700

    t

--- streamlit.py
+++ streamlit.py
@@ -3,7 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-
+st.write('Test')
 data = conn.read(worksheet='Test')
 df=pd.DataFrame(data)
 df=df.dropna(how='all')
